8e7f4ad0ca2761b815856c4e822616950d4d3ef8
give your user name here <your.email.id>
2021-09-21 00:30:12 +0300

    ...

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class CategoriesController extends Controller
 {
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
+// use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+// use Illuminate\Routing\Controller;
 
 class ProductsController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $products = Product::query()->with('category')->paginate(10);
+       return view('products/index', compact('products'));
+    }
+
+    public function show(Product $product)
+    {
+return view('products/show', compact('product'));
+    }
 }
