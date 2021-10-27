80a033c812dc6c043d1b7fea6dbae20dbed28e05
give your user name here <your.email.id>
2021-10-28 00:33:24 +0300

    test

--- tests/Feature/CategoriesControllerTest.php
+++ tests/Feature/CategoriesControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\Admin\CategoriesController;
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +15,19 @@ class CategoriesControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+
+    public function test_index()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_store()
+    {
+        
+$categories = Category::query()->create()->first();
+$route = route('admin.categories');
+$response = $this->post($route);
     }
 }
